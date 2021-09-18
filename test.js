020dd922490fe4e74a4632d35676775bcc94139f
Dmitry Kolesnikov <kastaneda@gmail.com>
2021-09-18 11:03:00 +0300

    Misc random change

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(event) {
     var form = '<form action="" method="post"><p><textarea name="comment" cols="60" rows="5" placeholder="Напишіть свій коментар"></textarea><br><input type="submit"></p></form>';
-    var commentBlock = '<hr><p><em>Коментарів: 0.</em></p>' + form + '<hr>';
-    document.querySelector('main').insertAdjacentHTML('beforeend', commentBlock);
+    var commentBlock = '<aside><p><em>Коментарів: 0.</em></p>' + form + '</aside>';
+    document.querySelector('main').insertAdjacentHTML('afterend', commentBlock);
 });
